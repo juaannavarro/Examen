7bf531ad9e6142d75405fccc420ed32673a8a8dc
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-14 10:44:45 +0100

    cambio

--- JuegoDelMinion.py
+++ JuegoDelMinion.py
@@ -4,6 +4,13 @@ def juego_del_minion(s):
     vocales="AEIOU"
     mayusculas=s.upper()
     
+    for i in range (len(s)):
+        if s[i] not in vocales:
+            puntuacionStuart=puntuacionStuart+len(s[i])
+        else:
+            puntuacionKevin=puntuacionKevin+len(s[i])
+    
+    if 
 if __name__ == '__main__':
     s=input("Elija una palabra: ")
     juego_del_minion(s)
