2b080813884ed5b777b686acab22381fa7985302
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-14 12:06:39 +0100

    final

--- TorresVerticales.py
+++ TorresVerticales.py
@@ -3,50 +3,90 @@ import os
 import random
 import re
 import sys
+import pprint
 
 def verticalRooks(r1, r2):
-    comienza=2
-    for torre1,torre2 in zip(r1, r2):
-        distancia_entre_torres= abs(torre2-torre1)
-        ganador=" "
-        if comienza==2:
-            if distancia_entre_torres==1:
-                distancia_entre_torres==1
-                ganador=" Jugador 1 "
-                comienza=2
+    winner=''
+    chess=[]
+    for i in range(len(r1)):
+        fila = []
+        for j in range(len(r1)):
+            if r1[j]==i+1:
+                fila.append('T1')
+            elif r2[j]==i+1:
+                fila.append('T2')
             else:
-                ganador=" Jugador 2 "
-                comienza=1
-        else:
-            if distancia_entre_torres==1:
-                ganador=" Jugador 2 "
-                comienza=1
+                fila.append(' ')
+        chess.append(fila)
+    pprint.pprint(chess)
+    print
+    jugador_1 = 'T1'
+    jugador_2 = 'T2'
+    jugador=jugador_2
+    while True:
+        movimiento=False
+        for i in range(len(r1)):
+            for j in range(len(r1)):
+                if chess[i][j]==jugador:
+                    if i-1 > 0 and chess[i-1][j]==' ':
+                        print('el jugador '+jugador+' mueve a posicion ('+str(i-1)+','+str(j)+')')
+                        chess[i-1][j]=jugador
+                        chess[i][j]= ' '  
+                        movimiento = True
+                        if (jugador == jugador_2):
+                            jugador=jugador_1
+                        else:
+                            jugador=jugador_2
+                        break
+                    elif i+1 < len(r1) and chess[i+1][j]==' ':
+                        print('el jugador '+jugador+' mueve a posicion ('+str(i+1)+','+str(j)+')')
+                        chess[i+1][j]=jugador
+                        chess[i][j]= ' ' 
+                        movimiento = True
+                        if (jugador == jugador_2):
+                            jugador=jugador_1
+                        else:
+                            jugador=jugador_2
+                        break
+            if movimiento==True:
+                break
+        if movimiento==True:
+            continue
+        elif movimiento==False:
+            print('No es posible realizar más movimientos al jugador '+jugador)
+            if (jugador == jugador_1):
+                winner = 'player-2'
             else:
-                ganador=" Jugador 1 "
-                comienza=2
-    if ganador==" Jugador 1 ":
-        return "player_1"
-    else:
-        return "player_2"
-    
-        
+                winner = 'player-1'
+            break
+
+    print('ganador '+winner)
+    return winner
 
 if __name__ == '__main__':
- 
- fptr = open(os.environ['OUTPUT_PATH'], 'w')
- 
- t = int(input().strip())
- 
- for t_itr in range(t):
- n = int(input().strip())
- r1 = []
- for _ in range(n):
- r1_item = int(input().strip())
- r1.append(r1_item)
- r2 = []
- for _ in range(n):
- r2_item = int(input().strip())
- r2.append(r2_item)
- result = verticalRooks(r1, r2)
- fptr.write(result + '\n')
- fptr.close()
+    fptr = open('prueba', 'w')
+
+    t = int(input("Introduce numeros del 1 al 3: ").strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        r1 = []
+
+        for _ in range(n):
+            r1_item = int(input().strip())
+            r1.append(r1_item)
+
+        r2 = []
+
+        for _ in range(n):
+            r2_item = int(input().strip())
+            r2.append(r2_item)
+
+        result = verticalRooks(r1, r2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
+
